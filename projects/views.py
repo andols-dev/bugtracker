66549f62dee8af185996da40b2c161cbba7eb024
Andreas Olsson <andreasolsson2020@outlook.com>
2022-01-30 15:23:14 +0100

    add resolved bugs and resolved bugs count

--- projects/views.py
+++ projects/views.py
@@ -13,6 +13,13 @@ def project_detail(request, id):
     project = Project.objects.get(id=id)
     project_bugs = Bug.objects.all().filter(project_id=id)
     unresolved_bugs = project_bugs.filter(state='UN').count()
+    resolved_bugs = project_bugs.filter(state='RE').count()
 
+    print(resolved_bugs)
     print(unresolved_bugs)
-    return render(request, 'projects/project_detail.html', {'project': project,'unresolved_bugs':unresolved_bugs})
+    context = {
+        'project': project,
+       'unresolved_bugs': unresolved_bugs,
+       'resolved_bugs': resolved_bugs
+    }
+    return render(request, 'projects/project_detail.html', context)
